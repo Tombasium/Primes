a9fa678290ffd1305b6df50d745152f273b9a5ea
Tombasium <tommwalton@gmail.com>
2016-02-18 22:31:55 +0000

    Update primesARM.s
    First working version of the prime program for ARM assembly. Slow as you anything!

--- primesARM.s
+++ primesARM.s
@@ -43,143 +43,159 @@ _inc_fact:
 _is_prime:
 	MOV R3, #0
 	MOV R10, #0
-	LDR R9, =bill
+	LDR R12, =bill
+	LDR R9, [R12]
+	MOV R8, R6
+	MOV R4, R6
 	BL _chk_num
 	BL _print_digit
-	LDR R9, =cmill
+	MOV R10, #0
+	LDR R12, =cmill
+	LDR R9, [R12]
+	MOV R8, R4
 	BL _chk_num
 	BL _print_digit
-	LDR R9, =xmill
+	MOV R10, #0
+	LDR R12, =xmill
+	LDR R9, [R12]
+	MOV R8, R4
 	BL _chk_num
 	BL _print_digit
-	LDR R9, =mill
+	MOV R10, #0
+	LDR R12, =mill
+	LDR R9, [R12]
+	MOV R8, R4
 	BL _chk_num
 	BL _print_digit
-	LDR R9, =cm
+	MOV R10, #0
+	LDR R12, =cm
+	LDR R9, [R12]
+	MOV R8, R4
 	BL _chk_num
 	BL _print_digit
-	LDR R9, =xm
+	MOV R10, #0
+	LDR R12, =xm
+	LDR R9, [R12]
+	MOV R8, R4
 	BL _chk_num
 	BL _print_digit
-	LDR R9, =m
+	MOV R10, #0
+	LDR R12, =m
+	LDR R9, [R12]
+	MOV R8, R4
 	BL _chk_num
 	BL _print_digit
-	LDR R9, =c
+	MOV R10, #0
+	LDR R12, =c
+	LDR R9, [R12]
+	MOV R8, R4
 	BL _chk_num
 	BL _print_digit
-	LDR R9, =x
+	MOV R10, #0
+	LDR R12, =x
+	LDR R9, [R12]
+	MOV R8, R4
 	BL _chk_num
 	BL _print_digit
-	LDR R9, =i
+	MOV R10, #0
+	LDR R12, =i
+	LDR R9, [R12]
+	MOV R8, R4
 	BL _chk_num
 	BL _print_digit
+	MOV R10, #0
 	MOV R0, #1
 	LDR R1, =ending
-	MOV R2, #1
+	MOV R2, #10
 	MOV R7, #4
 	SWI 0
-	
+	@CMP R6, #1000
+	@BGE _exit
+	BAL _inc_num
+	@BAL _exit
+
 _chk_num:
-	MOV R8, R6
 	CMP R8, R9
 	SUBGT R8, R8, R9
-	ADDGT R10, #1
+	ADDGE R10, #1
 	MOVLE PC, R14
-	B chk_num
+	CMP R10, #10
+	SUBEQ R10, R10, #10
+	B _chk_num
 	
 _print_digit:
-	MOV R0, #1			@ Best get the output ready now
-	MOV R2, #1			
-	MOV R7, #4
+	STMIA SP!, {LR}
 	
-	CMP R10, #0			@ Check if there's anything to print - if there's nothing in R10 then out we go
-	ADDGT R3, #1		@ If there is incremement R3
-	CMP R3, #0			@ Because if R3 is 0 then also we quit
-	MOVEQ PC, R14
-	ADD R10, #10
-	CMP R10, #10
-	LDREQ R1, =num_0
-	SWIEQ 0
-	SUB R10, #1
-	CMP R10, #9
-	LDREQ R1, =num_9
-	SWIEQ 0
-	SUB R10, #1
-	CMP R10, #8
-	LDREQ R1, =num_8
-	SWIEQ 0
-	SUB R10, #1
-	CMP R10, #7
-	LDREQ R1, =num_7
-	SWIEQ 0
-	SUB R10, #1
-	CMP R10, #6
-	LDREQ R1, =num_6
-	SWIEQ 0
-	SUB R10, #1
-	CMP R10, #5
-	LDREQ R1, =num_5
-	SWIEQ 0
-	SUB R10, #1
-	CMP R10, #4
-	LDREQ R1, =num_4
-	SWIEQ 0
-	SUB R10, #1
-	CMP R10, #3
-	LDREQ R1, =num_3
-	SWIEQ 0
-	SUB R10, #1
-	CMP R10, #2
-	LDREQ R1, =num_2
-	SWIEQ 0
-	SUB R10, #1
+	EOR R11, R11
+	CMP R10, #0
+	MULGT R11, R9, R10
+	SUB R4, R4, R11
+	EOR R11, R11
+
+	LDR R11, =num	
+
+	CMP R10, #0
+	BLEQ _exec
 	CMP R10, #1
-	LDREQ R1, =num_1
-	SWIEQ 0
+	BLEQ _exec
+	CMP R10, #2
+	BLEQ _exec
+	CMP R10, #3
+	BLEQ _exec
+	CMP R10, #4
+	BLEQ _exec
+	CMP R10, #5
+	BLEQ _exec
+	CMP R10, #6
+	BLEQ _exec
+	CMP R10, #7
+	BLEQ _exec
+	CMP R10, #8
+	BLEQ _exec
+	CMP R10, #9
+	BLEQ _exec
+	LDMDB SP!, {LR}
+	MOV PC, R14
 	
-	
+_exec:
+	MOV R0, #1
+	MOV R2, #1
+	MOV R7, #4
+	ADD R10, #48
+	STR R10, [R11]
+	LDR R1, =num
+	SWI 0
+	SUB R10, #48
+	LDMDB SP!, {LR}
+	MOV PC, R14
+
+_exit:
+	MOV R7, #1
+	SWI 0
 	
 .data
 bill:	
-	.int #1000000000
+	.int 1000000000
 cmill:
-	.int #100000000
+	.int 100000000
 xmill:
-	.int #10000000
+	.int 10000000
 mill:
-	.int #1000000
+	.int 1000000
 cm:
-	.int #100000
+	.int 100000
 xm:
-	.int #10000
+	.int 10000
 m:
-	.int #1000
+	.int 1000
 c:
-	.int #100
+	.int 100
 x:
-	.int #10
+	.int 10
 i:
-	.int #1
+	.int 1
 
 ending:
 	.ascii " is Prime\n"
-num_0:
-	.ascii "0"
-num_9:
-	.ascii "9"
-num_8:
-	.ascii "8"
-num_7:
-	.ascii "7"
-num_6:
-	.ascii "6"
-num_5:
-	.ascii "5"
-num_4:
-	.ascii "4"
-num_3:
-	.ascii "3"
-num_2:
-	.ascii "2"
-num_1:
-	.ascii "1"
+num:
+	.ascii " "
